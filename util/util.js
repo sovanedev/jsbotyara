235a5b7b4763d8889a13d15b9fd6cc57a632a8b7
sovanedev <88293854+sovanedev@users.noreply.github.com>
2021-10-20 23:21:09 +0400

    Create util.js

---
+++ util/util.js
@@ -0,0 +1,10 @@
+exports.canModifyQueue = (member) => {
+  const { channelID } = member.voice;
+  const botChannel = member.guild.voice.channelID;
+
+  if (channelID !== botChannel) {
+    return;
+  }
+
+  return true;
+};
